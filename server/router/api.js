64abf04d4ec213a90623c488b3b2b5f83395e39f
周昊 <zhouhao1@corp.netease.com>
2018-07-30 17:47:54 +0800

    fix: 删除没用的代码

--- server/router/api.js
+++ server/router/api.js
@@ -1,17 +1,10 @@
 /**
  * Created by zh on 2018/6/5.
  */
-import * as exampleController from '../controller/exampleController'
 import * as designController from '../controller/designController'
 import KoaRouter from 'koa-router';
 
 const router = new KoaRouter();
-
-router.get('/example/:id', exampleController.getExampleById); // 查询example
-router.get('/examples', exampleController.getAllExamples); // 查询所有example
-router.post('/example', exampleController.createExample); // 创建example
-router.put('/example', exampleController.updateExample); // 更新example
-router.delete('/example/:id', exampleController.deleteExample); // 删除example
 
 router.post('/design', designController.createDesign); // 创建design
 router.get('/design', designController.getDesignById); // 查询design
