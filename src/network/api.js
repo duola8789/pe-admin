8b496f5653770f317fcc9d081cd436dc150e609d
周昊 <zhouhao1@corp.netease.com>
2018-08-21 11:00:35 +0800

    feat: 学习CRM，在request.js中统一维护网络请求

--- src/network/api.js
+++ src/network/api.js
@@ -2,17 +2,17 @@
  * Created by zh on 2018/6/5.
  */
 export const userAPI = {
-  login:    '/auth/login', // 登陆 -- POST,
-  create:   '/auth/user', // 注册 -- POST,、
-  find:     '/auth/user', // 查询 -- GET,
+  login:        '/auth/login', // 登陆 -- POST,
+  create:       '/auth/user', // 注册 -- POST,
+  find:         '/auth/user', // 查询 -- GET,
 };
 
-export const exampleAPI = {
-  find:       '/api/example',
-  findAll:    '/api/examples',
-  add:        '/api/example',
-  update:     '/api/example',
-  remove:     '/api/example',
-  upload:     '/api/upload/certificate', // 上传文件凭证 -- GET,
+export const designAPI = {
+  find:         '/api/design',
+  findAll:      '/api/designs',
+  add:          '/api/design',
+  update:       '/api/design',
+  remove:       '/api/design',
+  certificate:  '/api/upload/certificate', // 上传文件凭证 -- GET,
 };
 
---
+++ src/network/request.js
@@ -0,0 +1,38 @@
+/**
+ * Created by zh on 2018/8/20.
+ */
+import * as API from '@/network/api'
+import { get, post, del, put } from '@/helper/httpHelper'
+
+// 查询
+const find = async (params) =>
+  await get(API.designAPI.find, params);
+
+// 查询列表
+const findAll = async (params) =>
+  await get(API.designAPI.findAll, params);
+
+// 创建
+const add = async (params) =>
+  await post(API.designAPI.add, params);
+
+// 更新
+const update = async (params) =>
+  await put(API.designAPI.update, params);
+
+// 删除
+const remove = async (params) =>
+  await del(API.designAPI.remove, params);
+
+// 上传授权
+const certificate = async (params) =>
+  await get(API.designAPI.certificate, params);
+
+export const design = {
+  find,
+  findAll,
+  add,
+  update,
+  remove,
+  certificate
+};
