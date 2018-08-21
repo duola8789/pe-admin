5a4b74a4854ad024b2dad50dd42f3cfe84bda230
周昊 <zhouhao1@corp.netease.com>
2018-08-21 11:01:27 +0800

    feat: 引入js-cookie并使用

--- src/main.js
+++ src/main.js
@@ -4,10 +4,12 @@ import Vue from '@/helper/elementHelper'
 import App from './App'
 import router from './router'
 import Axios from 'axios';
+import Cookie from 'js-cookie'
 
 import '@/helper/httpHelper';
 
 Vue.prototype.$http = Axios;
+Vue.prototype.$cookie = Cookie;
 
 Vue.config.productionTip = false;
 
